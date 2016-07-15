3ed16cbd608d75b7de28f48fb18090eacc7073aa
Calvin Kwong <ckwong93@gmail.com>
2016-07-14 21:32:31 -0700

    populate data with faker in seeds, edit password->password_hash in migrate, configure environment to include all gems(bcrypt, faker, etc.)

--- db/migrate/20160714200812_users.rb
+++ db/migrate/20160714200812_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration
   def change
   	create_table :users do |t|
   		t.string :username, null: false
-  		t.string :password, null: false
+  		t.string :password_hash, null: false
   		t.string :email, null: false
   		
   		t.timestamps
