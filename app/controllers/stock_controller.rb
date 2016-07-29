3c6c951022c37a6ccafb2f3502abb59fd94f4031
Calvin Kwong <ckwong93@gmail.com>
2016-07-29 11:24:37 -0700

    add in stock ajax

---
+++ app/controllers/stock_controller.rb
@@ -0,0 +1,55 @@
+get "/stock/new" do
+	@stock = Stock.all
+	if request.xhr?
+		erb :'/stock/_new', layout: false
+	else
+		erb :'/stock/new'
+	end
+end
+
+post "/stock/new" do
+	@stock = Stock.create(ticker: params[:ticker])
+	if request.xhr?
+		erb :'/stock/new', layout: false
+	else
+		redirect '/stock/view'
+	end
+end
+
+
+# post "/stock/new" do
+# 	@stock = Stock.create(ticker: params[:ticker])
+# 	if request.xhr?
+# 		erb :'/stock/_view', layout: false
+# 	else
+# 		erb :'/stock/view'
+# 	end
+# end
+get "/stock/view" do
+	@stock = Stock.all
+	erb :'/stock/view'
+end	
+
+get '/stock/nyse' do
+	if request.xhr?
+		erb :'/stock/nyse', layout: false
+	else
+		erb :'/stock/nyse'
+	end
+end
+
+get '/stock/dj' do
+	if request.xhr?
+		erb :'/stock/dj', layout: false
+	else
+		erb :'/stock/dj'
+	end
+end
+
+get '/stock/nasdaq' do
+	if request.xhr?
+		erb :'/stock/nasdaq', layout: false
+	else
+		erb :'/stock/nasdaq'
+	end
+end
--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-require 'faker'
-
-20.times do
-	User.create(username: Faker::Internet.domain_word,
-	email: Faker::Internet.email,
-	password: Faker::Internet.password(3))
-end
